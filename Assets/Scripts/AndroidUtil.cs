57274d52c7505c1dd2176a9bca3bf9b76cd32b17
sunegg <sunegggs@gmail.com>
2020-06-11 15:38:26 +0800

    first commit

---
+++ Assets/Scripts/AndroidUtil.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public static class AndroidUtil {
+	public static string GetImei() {
+		AndroidJavaObject tm   = new AndroidJavaObject("android.telephony.TelephonyManager");
+		string            imei = tm.Call<string>("getDeviceId");
+		return imei;
+	}
+}
---
+++ Assets/Scripts/WebView.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class WebView : MonoBehaviour {
+	private static string Url { get; set; } = "http://sdk.panguhy.com/game/direct_login?pgcid=2&gameId=6&imei=";
+
+	void Start() {
+		Input.backButtonLeavesApp =  true;
+		Url                       += AndroidUtil.GetImei();
+		GetComponent<UniWebView>().Load(Url);
+	}
+}
