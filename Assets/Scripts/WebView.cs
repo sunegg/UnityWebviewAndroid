95e01475b754af5fbcc3d369859943a1ba802147
sunegg <sunegggs@gmail.com>
2020-06-14 22:12:27 +0800

    first commit

--- Assets/Scripts/WebView.cs
+++ Assets/Scripts/WebView.cs
@@ -2,10 +2,17 @@
 
 public class WebView : MonoBehaviour {
 	private static string Url { get; set; } = "http://sdk.panguhy.com/game/direct_login?pgcid=2&gameId=6&imei=";
+	WebViewObject         _webViewObject;
 
 	void Start() {
 		Input.backButtonLeavesApp =  true;
 		Url                       += AndroidUtil.GetImei();
-		GetComponent<UniWebView>().Load(Url);
+		Init();
+		_webViewObject.LoadURL(Url);
+		_webViewObject.SetVisibility(true);
 	}
-}
+	void Init() {
+		_webViewObject = (new GameObject("WebViewObject")).AddComponent<WebViewObject>();
+		_webViewObject.Init(transparent: true);
+	}
+} 
